e52b248be6381a8cc277af0175af173fd1d535d6
Letícia <72623771+LeticiaAires@users.noreply.github.com>
2023-11-27 12:04:14 +0100

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -1,40 +1,53 @@
 #include "constantheader.h"
 
-int main(void){
-    char command[MAX_SIZE];
+volatile sig_atomic_t exit_requested = 0;
+
+void handle_signal(int signal) {
+    if (signal == SIGINT) {
+        write(STDOUT_FILENO, BYE, strlen(BYE));
+        exit_requested = 1;
+    }
+}
+
+int main(void) {
+    char command[MAX_SIZE] = {0};
+    char display[MAX_SIZE] = {0};
+    int command_length;
     int pid;
     int status;
-    int command_length;
     int test;
-    
-    write(STDOUT_FILENO, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));
-    
-    while(1){
-        write(STDOUT_FILENO, REGULAR_PROMPT, strlen(REGULAR_PROMPT));
-        
+
+    signal(SIGINT, handle_signal);
+
+    write(STDOUT_FILENO, HELLO, strlen(HELLO));
+    write(STDOUT_FILENO, REGULAR_PROMPT, strlen(REGULAR_PROMPT));
+
+    while (1) {
         command_length = read(STDIN_FILENO, command, MAX_SIZE);
-        command[command_length-1] = '\0';
-        
+        command[command_length - 1] = '\0';
+
         test = strcmp(EXIT, command);
-        if(test==0){
+        if (test == 0 || command_length == 0 || exit_requested) {
             write(STDOUT_FILENO, BYE, strlen(BYE));
-            kill(getpid(),SIGINT);
+            break;
         }
-        
+
         pid = fork();
-        if(pid==0){
+        if (pid == 0) {
             execlp(command, command, NULL);
+            write(STDOUT_FILENO, ERR, strlen(ERR));
+            _exit(EXIT_FAILURE);
         } else {
             wait(&status);
-            if(WIFSIGNALED(status)){
-                sprintf(display, "%s%d%s", SIGNALED_PROMPT, WSTOPSIG(status), END_PROMPT);
+            if (WIFSIGNALED(status)) {
+                sprintf(display, "%s%d%s", SIGNALED_PROMPT, WTERMSIG(status), END_PROMPT);
                 write(STDOUT_FILENO, display, strlen(display));
-            } else {
-                if(WIFEXITED(status)){
-                    sprintf(display, "%s%d%s", EXITED_PROMPT, WEXITSTATUS(status), END_PROMPT);
-                    write(STDOUT_FILENO, display, strlen(display));
-                }
+            } else if (WIFEXITED(status)) {
+                sprintf(display, "%s%d%s", EXITED_PROMPT, WEXITSTATUS(status), END_PROMPT);
+                write(STDOUT_FILENO, display, strlen(display));
             }
-        }
-    }   
+        }
+    }
+
+    return 0;
 }
