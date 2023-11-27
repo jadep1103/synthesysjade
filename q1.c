7c1ce54a6543e1aa812ea74c93bfb5c03110f457
Letícia <72623771+LeticiaAires@users.noreply.github.com>
2023-11-27 11:21:47 +0100

    Delete q1.c

--- q1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include "constantheader.h"
-
-int main(){
-    write(STDIN_FILENO,WELCOME_MESSAGE,strlen(WELCOME_MESSAGE));
-    return 0;
-}
